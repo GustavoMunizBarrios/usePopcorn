a762fdb84fb08627aebccf63ef3e68cf71fb74ba
Gustavo Muñiz <j.gustavomb1984@hotmail.com>
2024-03-27 19:31:54 -0600

    Improving Reusability with Props

--- src/index.js
+++ src/index.js
@@ -8,6 +8,16 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating
+      maxRating={10}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      color="black"
+      size={24}
+      defaultRating={10}
+    />
   </React.StrictMode>
 );
